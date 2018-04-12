b90dfac12b48d240b0d7cdaea4068ccfe055c784
qiaoyuwen <543684504@qq.com>
2018-04-12 17:55:27 +0800

    update profile

--- lib/page/profile.dart
+++ lib/page/profile.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import '../components/foot_bar.dart';
 import '../style/style.dart';
 import '../components/head_bar.dart';
+import '../store/store.dart';
+import '../routes/routes.dart';
+import '../config//config.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -15,6 +18,41 @@ class ProfileState extends State<Profile> {
       new EdgeInsets.symmetric(horizontal: Style.gPadding, vertical: 10.0);
   final _personColor = Style.backgroundColor;
   final _infoDataPadding = new EdgeInsets.all(Style.gPadding);
+
+  String _username = '登录/注册';
+  String _mobile = '暂无绑定手机号';
+  int _balance = 0;
+  int _count = 0;
+  int _pointNumber = 0;
+  String _avatar = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _initData();
+  }
+
+  void _initData() {
+    var user = store.state.user;
+    if (user != null) {
+      setState(() {
+        _username = user.username;
+        _mobile = '暂无绑定手机号';
+        if (user.mobile != null && user.mobile.isNotEmpty) {
+          _mobile = user.mobile;
+        }
+        _balance = user.balance;
+        _count = user.giftAmount;
+        _pointNumber = user.point;
+        _avatar = user.avatar;
+      });
+    } else {
+      setState(() {
+        _username = '登录/注册';
+        _mobile = '暂无绑定手机号';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,57 +109,81 @@ class ProfileState extends State<Profile> {
   }
 
   PreferredSizeWidget _buildPersonDetail() {
-    return new PreferredSize(
-      preferredSize: const Size.fromHeight(80.0),
-      child: new Container(
-        color: Style.primaryColor,
-        padding: _personPadding,
-        child: new Row(
-          children: <Widget>[
-            new Icon(
-              Icons.account_circle,
-              color: _personColor,
-              size: 60.0,
-            ),
-            new Expanded(
-              child: new Padding(
-                padding: new EdgeInsets.only(left: 5.0),
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    new Text(
-                      '登录/注册',
-                      style: new TextStyle(
-                        fontSize: 20.0,
-                        color: _personColor,
-                      ),
-                    ),
-                    new Row(
-                      children: <Widget>[
-                        new Icon(
-                          Icons.smartphone,
-                          color: _personColor,
-                          size: 20.0,
-                        ),
-                        new Text(
-                          '暂无绑定手机号',
-                          style: new TextStyle(
-                            fontSize: 13.0,
-                            color: _personColor,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+    List<Widget> children = [];
+    if (_avatar.isNotEmpty) {
+      children.add(
+        new CircleAvatar(
+          backgroundImage: new NetworkImage('${Config.ImgBaseUrl}$_avatar'),
+          radius: 30.0,
+        )
+      );
+    } else {
+      children.add(
+        new Icon(
+          Icons.account_circle,
+          color: _personColor,
+          size: 60.0,
+        )
+      );
+    }
+    children.addAll([
+      new Expanded(
+        child: new Padding(
+          padding: new EdgeInsets.only(left: 5.0),
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(
+                _username,
+                style: new TextStyle(
+                  fontSize: 20.0,
+                  color: _personColor,
                 ),
               ),
-            ),
-            new Icon(
-              Icons.arrow_forward_ios,
-              color: _personColor,
-              size: 15.0,
-            ),
-          ],
+              new Row(
+                children: <Widget>[
+                  new Icon(
+                    Icons.smartphone,
+                    color: _personColor,
+                    size: 20.0,
+                  ),
+                  new Text(
+                    _mobile,
+                    style: new TextStyle(
+                      fontSize: 13.0,
+                      color: _personColor,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      new Icon(
+        Icons.arrow_forward_ios,
+        color: _personColor,
+        size: 15.0,
+      ),
+    ]);
+    return new PreferredSize(
+      preferredSize: const Size.fromHeight(80.0),
+      child: new GestureDetector(
+        onTap: () {
+          String path;
+          if (store.state.user == null) {
+            path = '/login';
+          } else {
+            path = '/profile/login';
+          }
+          Routes.router.navigateTo(context, path);
+        },
+        child: new Container(
+          color: Style.primaryColor,
+          padding: _personPadding,
+          child: new Row(
+            children: children,
+          ),
         ),
       ),
     );
@@ -133,19 +195,19 @@ class ProfileState extends State<Profile> {
       child: new Row(
         children: <Widget>[
           _buildInfoDataSingle(
-            count: '0.00',
+            count: _balance.toStringAsFixed(2),
             countColor: new Color(0xffff9900),
             unit: '元',
             desc: '我的余额',
           ),
           _buildInfoDataSingle(
-            count: '0',
+            count: '$_count',
             countColor: new Color(0xffff5f3e),
             unit: '个',
             desc: '我的优惠',
           ),
           _buildInfoDataSingle(
-            count: '0',
+            count: '$_pointNumber',
             countColor: new Color(0xff6ac20b),
             unit: '分',
             desc: '我的积分',
